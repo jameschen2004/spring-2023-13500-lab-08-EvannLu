81fff5ca9a7914a9eb51b93420d52384700a7ef6
Evan Lu <evan.l60@myhunter.cuny.edu>
2023-03-23 22:54:06 -0400

    updated

--- scale.cpp
+++ scale.cpp
@@ -2,75 +2,9 @@
 #include <cassert>
 #include <cstdlib>
 #include <fstream>
+#include "imageio.h"
 
 using namespace std;
-
-const int MAX_H = 512;
-const int MAX_W = 512;
-
-void readImage(std::string filename, int image[MAX_H][MAX_W], int &height, int &width) {
-	char c;
-	int x;
-	ifstream instr;
-	instr.open(filename);
-
-	// read the header P2
-	instr >> c;
-	assert(c == 'P');
-	instr >> c;
-	assert(c == '2');
-
-	// skip the comments (if any)
-	while ((instr>>ws).peek() == '#') {
-		instr.ignore(4096, '\n');
-	}
-
-	instr >> width;
-	instr >> height;
-
-	assert(width <= MAX_W);
-	assert(height <= MAX_H);
-	int max;
-	instr >> max;
-	assert(max == 255);
-
-	for (int row = 0; row < height; row++)
-		for (int col = 0; col < width; col++)
-			instr >> image[row][col];
-	instr.close();
-	return;
-}
-
-
-// Writes a PGM file
-// Need to provide the array data and the image dimensions
-void writeImage(std::string filename, int image[MAX_H][MAX_W], int height, int width) {
-	ofstream ostr;
-	ostr.open(filename);
-	if (ostr.fail()) {
-		cout << "Unable to write file\n";
-		exit(1);
-	};
-
-	// print the header
-	ostr << "P2" << endl;
-	// width, height
-	ostr << width << ' ';
-	ostr << height << endl;
-	ostr << 255 << endl;
-
-	for (int row = 0; row < height; row++) {
-		for (int col = 0; col < width; col++) {
-			assert(image[row][col] < 256);
-			assert(image[row][col] >= 0);
-			ostr << image[row][col] << ' ';
-			ostr << endl;
-		}
-	}
-	ostr.close();
-	return;
-}
-
 
 
 void writeImageX2(std::string filename, int image[MAX_H*2][MAX_W*2], int height, int width) {
@@ -100,16 +34,14 @@ void writeImageX2(std::string filename, int image[MAX_H*2][MAX_W*2], int height,
 	return;
 }
 
-
-int main() {
-
+void scale(std::string fileimg) {
     int height; 
     int width; 
     int image[MAX_H][MAX_W];
     int grid[MAX_H*2][MAX_W*2];
     
 
-    readImage("inImage.pgm", image, height, width);
+    readImage(fileimg, image, height, width);
 
     for(int row=0; row<height*2; row++) {
         for (int col=0; col<width*2; col++) {
@@ -117,8 +49,5 @@ int main() {
         }
     }
 
-    writeImageX2("outImage.pgm", grid, height*2, width*2);
-
-
-	return 0;
+    writeImageX2("picE.pgm", grid, height*2, width*2);
 }
